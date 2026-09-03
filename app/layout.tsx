4e82ac56baaa980ba7a2f57de00e6cbbac9548b3
superai <superai@Ananyas-MacBook-Pro.local>
2026-09-03 20:27:30 +0530

    Let website control color theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${sourceSerif.variable} ${inter.variable}`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light">
           <SiteHeader />
           {children}
           <SiteFooter />
